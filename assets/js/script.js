6a3965a25841d50178c19422cc33038fa4795ca0
elenaramirezhdz <>
2023-09-25 10:18:33 +0000

    Add border radius

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,118 @@
-const questionContainerElement = document.getElementById('game-container');
 const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
+const answerButtons = document.getElementById('answer-buttons');
+const scoreElement = document.getElementById('score');
+const totalAnsweredElement = document.getElementById('total-answered');
 
-// Scores variable
-const numberAnswered = document.getElementById('total-answered');
-const scoreBoard = document.getElementById('score');
-let totalAnswered = 0;
+
+const questions = [
+    {
+        question: 'What are the best touristic zones in Mexico City?',
+        answers: [
+            { text: 'Polanco, Coyacan, Lomas de Bezares', correct: false },
+            { text: 'Roma Norte, Condesa, Polanco, Coyacan', correct: true },
+            { text: 'Oasis, Tacuba, Padierna, Polanco', correct: false },
+            { text: 'Iztaccihuatl, Bosque de las Lomas', correct: false },
+        ]
+    },
+    {
+        question: 'What is a famous cafe and a book shop in CDMX?',
+        answers: [
+            { text: 'Peltre', correct: false },
+            { text: 'Terra Garrat', correct: false },
+            { text: 'Pendulo', correct: true },
+            { text: 'Mague Cafe', correct: false },
+        ]
+    },
+    {
+        question: 'What are the names of the book shops in CDMX?',
+        answers: [
+            { text: 'Gandhi, Peltre', correct: false },
+            { text: 'Sotano, Gandhi', correct: true },
+            { text: 'Frida Cahlo, Garibaldi', correct: false },
+            { text: 'Sears, Soriana', correct: false },
+        ]
+    },
+    {
+        question: 'What is one of the famous universities that has its own university city in CDMX? ',
+        answers: [
+            { text: 'Colmex', correct: false },
+            { text: 'UNAM', correct: true },
+            { text: 'ITAM', correct: false },
+            { text: 'ITESM', correct: false },
+        ]
+    },
+    {
+        question: 'What is the famous Mariachi square in CDMX?',
+        answers: [
+            { text: 'Garibaldi', correct: true },
+            { text: 'Jamaica', correct: false },
+            { text: 'Condesa', correct: false },
+            { text: 'Juarez', correct: false },
+        ]
+    },
+    {
+        question: 'What is the name of the park where there are many dance classes on weekends?',
+        answers: [
+            { text: 'Parque España', correct: false },
+            { text: 'Parque México', correct: true },
+            { text: 'Parque Hundido', correct: false },
+            { text: 'Xochimilco', correct: false },
+        ]
+    },;
+
+let currentQuestionIndex = 0;
 let score = 0;
-scoreBoard.innerHTML = score;
-numberAnswered.innerHTML = totalAnswered;
+let totalAnswered = 0;
 
-//variable to check the questions 
-let questionAnswered = false;
+function startGame() {
+    currentQuestionIndex = 0;
+    score = 0;
+    totalAnswered = 0;
+    setNextQuestion();
+}
 
-let shuffledQuestions, currentQuestionIndex;
+function setNextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        const currentQuestion = questions[currentQuestionIndex];
+        questionElement.innerText = currentQuestion.question;
+        resetButtonColors();
+        displayAnswers(currentQuestion.answers);
+    } else {
+        // Quiz is over
+        alert('Quiz completed! Your score: ' + score + '/' + questions.length);
+        startGame();
+    }
+}
 
-/*Bringing a question from the array and setting its text and creating the option buttons
-with the right text-content and the "correct" dataset to let you know if the answer is correct*/
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
+function resetButtonColors() {
+    const buttons = Array.from(answerButtons.children);
+    buttons.forEach(button => {
+        button.classList.remove('correct');
+        button.classList.remove('incorrect');
     });
+}
 
+function displayAnswers(answers) {
+    const buttons = Array.from(answerButtons.children);
+    buttons.forEach((button, index) => {
+        button.innerText = answers[index].text;
+        button.onclick = () => checkAnswer(answers[index].correct);
+    });
+}
 
-    const questions = [
-        {
-            question: 'What are the best touristic zones in Mexico City?',
-            answers: [
-                { text: 'Polanco, Coyacan, Lomas de Bezares', correct: false },
-                { text: 'Roma Norte, Condesa, Polanco, Coyacan', correct: true },
-                { text: 'Oasis, Tacuba, Padierna, Polanco', correct: false },
-                { text: 'Iztaccihuatl, Bosque de las Lomas', correct: false },
-            ]
-        },
-        {
-            question: 'What is a famous cafe and a book shop in CDMX?',
-            answers: [
-                { text: 'Peltre', correct: false },
-                { text: 'Terra Garrat', correct: false },
-                { text: 'Pendulo', correct: true },
-                { text: 'Mague Cafe', correct: false },
-            ]
-        },
-        {
-            question: 'What are the names of the book shops in CDMX?',
-            answers: [
-                { text: 'Gandhi, Peltre', correct: false },
-                { text: 'Sotano, Gandhi', correct: true },
-                { text: 'Frida Cahlo, Garibaldi', correct: false },
-                { text: 'Sears, Soriana', correct: false },
-            ]
-        },
-        {
-            question: 'What is one of the famous universities that has its own university city in CDMX? ',
-            answers: [
-                { text: 'Colmex', correct: false },
-                { text: 'UNAM', correct: true },
-                { text: 'ITAM', correct: false },
-                { text: 'ITESM', correct: false },
-            ]
-        },
-        {
-            question: 'What is the famous Mariachi square in CDMX?',
-            answers: [
-                { text: 'Garibaldi', correct: true },
-                { text: 'Jamaica', correct: false },
-                { text: 'Condesa', correct: false },
-                { text: 'Juarez', correct: false },
-            ]
-        },
-        {
-            question: 'What is the name of the park where there are many dance classes on weekends?',
-            answers: [
-                { text: 'Parque España', correct: false },
-                { text: 'Parque México', correct: true },
-                { text: 'Parque Hundido', correct: false },
-                { text: 'Xochimilco', correct: false },
-            ]
-        },;
+function checkAnswer(isCorrect) {
+    totalAnswered++;
+    if (isCorrect) {
+        score++;
+        answerButtons.children[currentQuestionIndex].classList.add('correct');
+    } else {
+        answerButtons.children[currentQuestionIndex].classList.add('incorrect');
+    }
+
+    scoreElement.innerText = score;
+    totalAnsweredElement.innerText = totalAnswered;
+    currentQuestionIndex++;
+    setTimeout(setNextQuestion, 1000); // Delay to show the correct/incorrect colors
+}
+
+startGame(); // Start the game when the page loads
